c528d095aff8f178189263490dcd47ade10788c3
atp1o2 <andrewpham8@gmail.com>
2015-11-12 11:15:05 -0800

    clean up old code. Need order info to complete extension

--- content.js
+++ content.js
@@ -1,34 +1,11 @@
-  // var element = document.getElementsByClassName('vcard-names');
-
-  // if(element.length > 0 && element){
-  //     alert('Element Exists');
-  //   } else {
-  //     alert('Element does NOT exists');
-  //     // setTimeout(function(){window.location.reload(1);}, 50);
-  // };
-
-
-  // var element = document.getElementsByClassName('vcard-names');
-
-  //   if(element.length > 0 && element){
-  //     alert('Element Exists');
-  //   } else {
-  //     alert('Element does NOT exists');
-  //     // setTimeout(function(){window.location.reload(1);}, 5000);
-  //   };
-
-// alert("Hello from " + document.location)
-
-    if(document.location == "https://github.com/atp1o2"){
-      var element = window.document.getElementsByClassName('vcard-names');
-      if(element.length > 0 && element){
-        alert('Element Exists');
-        element.focus();
-      } else {
-        alert('Element does NOT exists');
-        setTimeout(function(){window.location.reload(true);}, 3000);
-      };
-    };
+if(document.location == "https://github.com/atp1o2"){
+  var element = window.document.getElementsByClassName('vcard-names');
+  if(element.length > 0 && element){
+    alert('Order Found!');
+  } else {
+    setTimeout(function(){window.location.reload(true);}, Math.random()*10000);
+  };
+};
 
 
 
